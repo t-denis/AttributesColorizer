60f6b6f4c776d7f5aa2e0fa1dd2281c0f872eb06
TDenis <dtimukhin@yandex.ru>
2015-08-23 16:22:33 +0300

    Add new EditorClassifier: AttributeClassifier

---
+++ AttributesColorizer/AttributeClassifierFormat.cs
@@ -0,0 +1,35 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="AttributeClassifierFormat.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System.ComponentModel.Composition;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace AttributesColorizer
+{
+    /// <summary>
+    /// Defines an editor format for the AttributeClassifier type that has a purple background
+    /// and is underlined.
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "AttributeClassifier")]
+    [Name("AttributeClassifier")]
+    [UserVisible(true)] // This should be visible to the end user
+    [Order(Before = Priority.Default)] // Set the priority to be after the default classifiers
+    internal sealed class AttributeClassifierFormat : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AttributeClassifierFormat"/> class.
+        /// </summary>
+        public AttributeClassifierFormat()
+        {
+            this.DisplayName = "AttributeClassifier"; // Human readable version of the name
+            this.BackgroundColor = Colors.BlueViolet;
+            this.TextDecorations = System.Windows.TextDecorations.Underline;
+        }
+    }
+}
