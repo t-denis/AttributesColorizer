27b582a4d8dcb0b2afa42d3b7506fe57f0bfd660
TDenis <dtimukhin@yandex.ru>
2015-08-23 18:35:43 +0300

    Replace test project (didn't know that VSIX can`t be referenced). Add class library for code that need to be tested. Add default roslyn-based attribute search implementation and few tests.

---
+++ AttributesColorizer.Core.Tests/CodeParserTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using JetBrains.Annotations;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AttributesColorizer.Core.Tests
+{
+    [TestClass]
+    public class CodeParserTests
+    {
+        [TestMethod]
+        [DeploymentItem(@"Samples\SingleAttribute.txt", "Samples")]
+        public void SingleAttribute()
+        {
+            var csCode = GetCode(@"Samples\SingleAttribute.txt");
+            var syntaxTree = CSharpSyntaxTree.ParseText(csCode);
+
+            var codeParser = new CodeParser();
+            var attributes = codeParser.GetAttributes(syntaxTree).ToList();
+
+            Assert.AreEqual(1, attributes.Count);
+            var startPosition = csCode.IndexOf("[Serializable]", StringComparison.Ordinal);
+            Assert.AreEqual(startPosition, attributes.Single().Start);
+
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"Samples\CustomAttribute.txt", "Samples")]
+        public void CustomAttribute()
+        {
+            string file = @"Samples\CustomAttribute.txt";
+            Assert.IsTrue(File.Exists(file), $"deployment failed: {file} did not get deployed");
+
+            var csCode = File.ReadAllText(file);
+            var syntaxTree = CSharpSyntaxTree.ParseText(csCode);
+
+            var codeParser = new CodeParser();
+            var attributes = codeParser.GetAttributes(syntaxTree).ToList();
+
+            Assert.AreEqual(1, attributes.Count);
+            var startPosition = csCode.IndexOf("[Test]", StringComparison.Ordinal);
+            Assert.AreEqual(startPosition, attributes.Single().Start);
+        }
+
+        [NotNull]
+        private static string GetCode([NotNull] string file)
+        {
+            Assert.IsTrue(File.Exists(file), $"deployment failed: {file} did not get deployed");
+
+            var csCode = File.ReadAllText(file);
+            return csCode;
+        }
+    }
+}
---
+++ AttributesColorizer.Core/CodeParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace AttributesColorizer.Core
+{
+    public class CodeParser
+    {
+        [NotNull]
+        public IEnumerable<TextSpan> GetAttributes([NotNull] SyntaxTree syntaxTree)
+        {
+            if (syntaxTree == null) throw new ArgumentNullException(nameof(syntaxTree));
+            return syntaxTree.GetRoot()
+               .DescendantNodes()
+               .OfType<AttributeListSyntax>()
+               .Select(x => x.Span);
+        }
+    }
+}
