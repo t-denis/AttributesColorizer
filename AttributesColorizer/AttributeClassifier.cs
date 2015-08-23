4c1f9b429bb13811da3b75d5c2d616fd14326888
TDenis <dtimukhin@yandex.ru>
2015-08-23 20:45:09 +0300

    Added parsing result cache. VS executes classifier multiple times for the same text snapshot, but with different spans. No need to run parsing every time.

--- AttributesColorizer/AttributeClassifier.cs
+++ AttributesColorizer/AttributeClassifier.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
 using AttributesColorizer.Core;
 using Microsoft.CodeAnalysis.Text;
 using Microsoft.VisualStudio.Text;
@@ -10,6 +12,7 @@ namespace AttributesColorizer
     internal class AttributeClassifier : IClassifier
     {
         private readonly IClassificationType _classificationType;
+        private ParsingResult _lastParsingResult;
 
         internal AttributeClassifier(IClassificationTypeRegistryService registry)
         {
@@ -17,34 +20,40 @@ namespace AttributesColorizer
         }
 
         /// <summary>
-        /// An event that occurs when the classification of a span of text has changed.
+        ///     An event that occurs when the classification of a span of text has changed.
         /// </summary>
         /// <remarks>
-        /// This event gets raised if a non-text change would affect the classification in some way,
-        /// for example typing /* would cause the classification to change in C# without directly
-        /// affecting the span.
+        ///     This event gets raised if a non-text change would affect the classification in some way,
+        ///     for example typing /* would cause the classification to change in C# without directly
+        ///     affecting the span.
         /// </remarks>
         public event EventHandler<ClassificationChangedEventArgs> ClassificationChanged;
 
         /// <summary>
-        /// Gets all the <see cref="ClassificationSpan"/> objects that intersect with the given range of text.
+        ///     Gets all the <see cref="ClassificationSpan" /> objects that intersect with the given range of text.
         /// </summary>
         /// <remarks>
-        /// This method scans the given SnapshotSpan for potential matches for this classification.
-        /// In this instance, it classifies everything and returns each span as a new ClassificationSpan.
+        ///     This method scans the given SnapshotSpan for potential matches for this classification.
+        ///     In this instance, it classifies everything and returns each span as a new ClassificationSpan.
         /// </remarks>
         /// <param name="span">The span currently being classified.</param>
         /// <returns>A list of ClassificationSpans that represent spans identified to be of this classification.</returns>
         public IList<ClassificationSpan> GetClassificationSpans(SnapshotSpan span)
         {
-            var result = new List<ClassificationSpan>();
-            var document = span.Snapshot.GetOpenDocumentInCurrentContextWithChanges();
-            var syntaxTree = document.GetSyntaxTreeAsync().Result;
+            IReadOnlyCollection<TextSpan> attributeSpans;
+            if (_lastParsingResult != null && _lastParsingResult.TextSnapshot == span.Snapshot)
+            {
+                attributeSpans = _lastParsingResult.AttributeSpans;
+            }
+            else
+            {
+                attributeSpans = ParseAttributes(span.Snapshot);
+                _lastParsingResult = new ParsingResult(span.Snapshot, attributeSpans);
+            }
 
-            var codeParser = new CodeParser();
-            var attributes = codeParser.GetAttributeLists(syntaxTree);
+            var result = new List<ClassificationSpan>();
             var roslynSpan = new TextSpan(span.Start, span.Length);
-            foreach (var attribute in attributes)
+            foreach (var attribute in attributeSpans)
             {
                 if (attribute.OverlapsWith(roslynSpan))
                 {
@@ -53,8 +62,18 @@ namespace AttributesColorizer
                     result.Add(classificationSpan);
                 }
             }
-            
+
             return result;
+        }
+
+        private static IReadOnlyCollection<TextSpan> ParseAttributes(ITextSnapshot snapshot)
+        {
+            var document = snapshot.GetOpenDocumentInCurrentContextWithChanges();
+            var syntaxTree = document.GetSyntaxTreeAsync().Result;
+
+            var codeParser = new CodeParser();
+            var attributes = codeParser.GetAttributeLists(syntaxTree);
+            return attributes.ToImmutableList();
         }
     }
 }
---
+++ AttributesColorizer/ParsingResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio.Text;
+
+namespace AttributesColorizer
+{
+    public class ParsingResult
+    {
+        public ParsingResult(ITextSnapshot textSnapshot,
+            IEnumerable<TextSpan> attributeSpans)
+        {
+            TextSnapshot = textSnapshot;
+            AttributeSpans = attributeSpans.ToImmutableArray();
+        }
+
+        public ITextSnapshot TextSnapshot { get; }
+        public IReadOnlyCollection<TextSpan> AttributeSpans { get; }
+    }
+}
