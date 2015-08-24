0b917de94d702ad42397867b29ddb0e7fd2bd612
TDenis <dtimukhin@yandex.ru>
2015-08-24 20:33:11 +0300

    Add options page

---
+++ DarkAttributes/DarkAttributesOptionsPage.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using Microsoft.VisualStudio.Shell;
+
+namespace DarkAttributes
+{
+    public class DarkAttributesOptionsPage : DialogPage
+    {
+        [Category("Font settings")]
+        [DisplayName("Foreground opacity")]
+        [Description("Integer value between 0 (transparent) and 100 (opaque).")]
+        public int Opacity { get; set; } = 35;
+    }
+}
--- DarkAttributes/DarkAttributesPackage.cs
+++ DarkAttributes/DarkAttributesPackage.cs
@@ -39,6 +39,7 @@ namespace DarkAttributes
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [Guid(DarkAttributesPackage.PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    [ProvideOptionPage(typeof(DarkAttributesOptionsPage), "DarkAttributes", "General", 0, 0, true)]
     public sealed class DarkAttributesPackage : Package
     {
         /// <summary>
