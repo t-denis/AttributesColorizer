1f9a29c0648507e6c37df9f5782726c8f61163c3
TDenis <dtimukhin@yandex.ru>
2015-08-24 22:00:50 +0300

    Add ExtensionSettings class that provides access to settings via DTE. See for details: https://msdn.microsoft.com/en-us/library/bb166195(v=vs.120).aspx

--- DarkAttributes/DarkAttributesOptionsPage.cs
+++ DarkAttributes/DarkAttributesOptionsPage.cs
@@ -1,13 +1,17 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell;
 
 namespace DarkAttributes
 {
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [CLSCompliant(false), ComVisible(true)]
     public class DarkAttributesOptionsPage : DialogPage
     {
-        [Category("Font settings")]
+        [Category("Settings")]
         [DisplayName("Foreground opacity")]
         [Description("Integer value between 0 (transparent) and 100 (opaque).")]
-        public int Opacity { get; set; } = 35;
+        public int ForegroundOpacity { get; set; } = 35;
     }
 }
