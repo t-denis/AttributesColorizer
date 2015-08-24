6b9a877ffa117cad4c4f04e0e6ad34bb900edcac
TDenis <dtimukhin@yandex.ru>
2015-08-24 23:26:00 +0300

    Add TextPropertiesService - a class that can set font opacity for attributes at runtime. See for details: http://stackoverflow.com/questions/25860706/export-custom-editorformatdefinition-at-runtime

---
+++ DarkAttributes/TextPropertiesService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.Text.Classification;
+
+namespace DarkAttributes
+{
+    public class TextPropertiesService
+    {
+        private readonly IClassificationTypeRegistryService _registryService;
+        private readonly IClassificationFormatMapService _classificationFormatMapService;
+
+        public TextPropertiesService(IClassificationTypeRegistryService registryService,
+            IClassificationFormatMapService classificationFormatMapService)
+        {
+            _registryService = registryService;
+            _classificationFormatMapService = classificationFormatMapService;
+        }
+
+        public void SetForegroundOpacity(double opacity)
+        {
+            var classificationType = _registryService.GetClassificationType(Constants.AttributeClassificationTypeName);
+            var classificationFormatMap = _classificationFormatMapService.GetClassificationFormatMap("CSharp");
+            var currentProperties = classificationFormatMap.GetExplicitTextProperties(classificationType);
+            if (currentProperties.ForegroundOpacity == opacity)
+                return;
+            var newProperties = currentProperties.SetForegroundOpacity(opacity);
+            classificationFormatMap.AddExplicitTextProperties(classificationType, newProperties);
+        }
+    }
+}
