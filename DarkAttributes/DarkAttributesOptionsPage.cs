8abccc49c5f3d308cb1ede7d91cb3372502cc930
TDenis <dtimukhin@yandex.ru>
2015-08-25 20:54:30 +0300

    Add opacity modification using settings page.

--- DarkAttributes/DarkAttributesOptionsPage.cs
+++ DarkAttributes/DarkAttributesOptionsPage.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.ComponentModelHost;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text.Classification;
 
 namespace DarkAttributes
 {
@@ -9,9 +11,34 @@ namespace DarkAttributes
     [CLSCompliant(false), ComVisible(true)]
     public class DarkAttributesOptionsPage : DialogPage
     {
+        private readonly TextPropertiesService _textPropertiesService;
+        private int _foregroundOpacity;
+
+        public DarkAttributesOptionsPage()
+        {
+            var componentModel = (IComponentModel)Package.GetGlobalService(typeof(SComponentModel));
+            var classicationFormatMapService = componentModel.GetService<IClassificationFormatMapService>();
+            var classificationTypeRegistryService = componentModel.GetService<IClassificationTypeRegistryService>();
+            _textPropertiesService = new TextPropertiesService(classificationTypeRegistryService, classicationFormatMapService);
+        }
+
         [Category("Settings")]
         [DisplayName("Foreground opacity")]
         [Description("Integer value between 0 (transparent) and 100 (opaque).")]
-        public int ForegroundOpacity { get; set; } = 35;
+        public int ForegroundOpacity
+        {
+            get { return _foregroundOpacity; }
+            set { _foregroundOpacity = value; }
+        }
+
+        public override void SaveSettingsToStorage()
+        {
+            _textPropertiesService.SetForegroundOpacity(_foregroundOpacity / 100.0);
+        }
+
+        public override void LoadSettingsFromStorage()
+        {
+            _foregroundOpacity = Convert.ToInt32(_textPropertiesService.GetForegroundOpacity() * 100.0);
+        }
     }
 }
--- DarkAttributes/TextPropertiesService.cs
+++ DarkAttributes/TextPropertiesService.cs
@@ -4,25 +4,30 @@ namespace DarkAttributes
 {
     public class TextPropertiesService
     {
-        private readonly IClassificationTypeRegistryService _registryService;
-        private readonly IClassificationFormatMapService _classificationFormatMapService;
+        private readonly IClassificationFormatMap _classificationFormatMap;
+        private readonly IClassificationType _classificationType;
 
         public TextPropertiesService(IClassificationTypeRegistryService registryService,
             IClassificationFormatMapService classificationFormatMapService)
         {
-            _registryService = registryService;
-            _classificationFormatMapService = classificationFormatMapService;
+            _classificationFormatMap = classificationFormatMapService.GetClassificationFormatMap("text");
+            _classificationType = registryService.GetClassificationType(Constants.AttributeClassificationTypeName);
+        }
+
+        public double GetForegroundOpacity()
+        {
+            var currentProperties = _classificationFormatMap.GetExplicitTextProperties(_classificationType);
+            return currentProperties.ForegroundOpacity;
         }
 
         public void SetForegroundOpacity(double opacity)
         {
-            var classificationType = _registryService.GetClassificationType(Constants.AttributeClassificationTypeName);
-            var classificationFormatMap = _classificationFormatMapService.GetClassificationFormatMap("CSharp");
-            var currentProperties = classificationFormatMap.GetExplicitTextProperties(classificationType);
+            var currentProperties = _classificationFormatMap.GetExplicitTextProperties(_classificationType);
             if (currentProperties.ForegroundOpacity == opacity)
                 return;
             var newProperties = currentProperties.SetForegroundOpacity(opacity);
-            classificationFormatMap.AddExplicitTextProperties(classificationType, newProperties);
+
+            _classificationFormatMap.AddExplicitTextProperties(_classificationType, newProperties);
         }
     }
 }
